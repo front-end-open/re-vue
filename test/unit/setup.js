2cbedd1a3d1739b92f9f777e0bb2490b4c9cca5c
wangshan <22212@qq.com>
2022-06-27 00:05:54 +0800

    feat: 增加测试1[failed]

---
+++ test/unit/setup.js
@@ -0,0 +1,10 @@
+/*
+ * @LastEditTime: 2022-06-26 23:50:32
+ * @Description: 单测入口
+ * @Date: 2022-06-26 23:50:32
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
+import Vue from "vue";
+
+Vue.config.productionTip = false;
