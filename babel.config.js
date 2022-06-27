b8a951260069596b44f78bd4e0b813bf4cd900fb
wangshan <22212@qq.com>
2022-06-27 16:45:55 +0800

    chore: 设置babel预设环境

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,18 @@
 /*
- * @LastEditTime: 2022-05-31 23:38:48
+ * @LastEditTime: 2022-06-27 16:45:25
  * @Description:
  * @Date: 2022-05-25 00:55:34
  * @Author: wangshan
  * @LastEditors: wangshan
  */
+//@vue/cli-plugin-babel/preset提供对babel和兴库和babel-loader的预设
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: [
+    [
+      "@vue/cli-plugin-babel/preset",
+      {
+        targets: { node: "current" }, // 设置编译的目标环境,默认环境取决于broserlint
+      },
+    ],
+  ],
 };
