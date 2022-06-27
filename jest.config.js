64b96e9b64c2731381b3126a9ed68260ec5af850
wangshan <22212@qq.com>
2022-06-27 16:01:01 +0800

    chore: 测试环境更新

--- jest.config.js
+++ jest.config.js
@@ -201,17 +201,17 @@ module.exports = {
     // 类似 webpack.resolve.extensions
     "js",
     "json",
-    "vue",
+    // "vue",
   ],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1", // 类似 webpack.resolve.alias
   },
   transform: {
     // 类似 webpack.module.rules
-    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest", // 为Jest提供最新的Es2015语法
     ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest",
   },
-  setupFiles: ["<rootDir>/test/unit/setup"], // 类似 webpack.entry
+  setupFiles: ["<rootDir>/test/unit/setup.js"], // 类似 webpack.entry
   coverageDirectory: "<rootDir>/test/unit/coverage", // 类似 webpack.output
   collectCoverageFrom: [
     // 类似 webpack 的 rule.include
--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,10 +1,10 @@
 /*
- * @LastEditTime: 2022-06-26 23:50:32
+ * @LastEditTime: 2022-06-27 15:56:36
  * @Description: 单测入口
  * @Date: 2022-06-26 23:50:32
  * @Author: wangshan
  * @LastEditors: wangshan
  */
-import Vue from "vue";
+// import Vue from "vue";
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
